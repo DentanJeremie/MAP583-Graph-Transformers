ba1521d3db65c400284f7bfa3e6b22d0235fcb83
Jérémie Dentan <jeremie.dentan@live.com>
2022-03-03 21:27:32 +0100

    config

--- scripts/SBMs/script_main_SBMs_node_classification_PATTERN_500k_LaplRenorm.sh
+++ scripts/SBMs/script_main_SBMs_node_classification_PATTERN_500k_LaplRenorm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 code=main_SBMs_node_classification.py 
-python $code --config 'configs/SBMs_GraphTransformer_LapPE_PATTERN_500k_sparse_graph_BN.json' --renormalization_pos_enc 0.01 --batch_size 20
-python $code --config 'configs/SBMs_GraphTransformer_LapPE_PATTERN_500k_sparse_graph_BN.json' --renormalization_pos_enc 0.3 --batch_size 20
-python $code --config 'configs/SBMs_GraphTransformer_LapPE_PATTERN_500k_sparse_graph_BN.json' --renormalization_pos_enc 0.7 --batch_size 20
-python $code --config 'configs/SBMs_GraphTransformer_LapPE_PATTERN_500k_sparse_graph_BN.json' --renormalization_pos_enc 5.0 --batch_size 20
-python $code --config 'configs/SBMs_GraphTransformer_LapPE_PATTERN_500k_sparse_graph_BN.json' --renormalization_pos_enc 20.0 --batch_size 20
-python $code --config 'configs/SBMs_GraphTransformer_LapPE_PATTERN_500k_sparse_graph_BN.json' --renormalization_pos_enc 100.0 --batch_size 20
+python $code --config 'configs/SBMs_GraphTransformer_LapPE_PATTERN_500k_sparse_graph_BN.json' --renormalization_pos_enc 0.01 --batch_size 20 --epochs 20
+python $code --config 'configs/SBMs_GraphTransformer_LapPE_PATTERN_500k_sparse_graph_BN.json' --renormalization_pos_enc 0.3 --batch_size 20 --epochs 20
+python $code --config 'configs/SBMs_GraphTransformer_LapPE_PATTERN_500k_sparse_graph_BN.json' --renormalization_pos_enc 0.7 --batch_size 20 --epochs 20
+python $code --config 'configs/SBMs_GraphTransformer_LapPE_PATTERN_500k_sparse_graph_BN.json' --renormalization_pos_enc 5.0 --batch_size 20 --epochs 20
+python $code --config 'configs/SBMs_GraphTransformer_LapPE_PATTERN_500k_sparse_graph_BN.json' --renormalization_pos_enc 20.0 --batch_size 20 --epochs 20
+python $code --config 'configs/SBMs_GraphTransformer_LapPE_PATTERN_500k_sparse_graph_BN.json' --renormalization_pos_enc 100.0 --batch_size 20 --epochs 20
