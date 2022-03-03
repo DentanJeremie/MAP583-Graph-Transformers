e519b756146aa4e0360757c727aeb197416be517
Jérémie Dentan <jeremie.dentan@live.com>
2022-03-03 18:37:44 +0100

    script 1

--- scripts/SBMs/script_main_SBMs_node_classification_PATTERN_500k_LaplRenorm.sh
+++ scripts/SBMs/script_main_SBMs_node_classification_PATTERN_500k_LaplRenorm.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 code=main_SBMs_node_classification.py 
 python $code --config 'configs/ablation_SBMs_GraphTransformer_WLPE_PATTERN_500k_sparse_graph_BN.json' --renormalization_pos_enc 0.2
+python $code --config 'configs/ablation_SBMs_GraphTransformer_WLPE_PATTERN_500k_sparse_graph_BN.json' --renormalization_pos_enc 0.1
