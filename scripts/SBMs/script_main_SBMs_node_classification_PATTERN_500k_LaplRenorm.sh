cf4b11141f35e78a4f3731676a1a4756f66d0528
Jérémie Dentan <jeremie.dentan@live.com>
2022-03-03 18:42:16 +0100

    script 1

--- scripts/SBMs/script_main_SBMs_node_classification_PATTERN_500k_LaplRenorm.sh
+++ scripts/SBMs/script_main_SBMs_node_classification_PATTERN_500k_LaplRenorm.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 code=main_SBMs_node_classification.py 
-python $code --config 'configs/ablation_SBMs_GraphTransformer_WLPE_PATTERN_500k_sparse_graph_BN.json' --renormalization_pos_enc 0.2
-python "print(\n\n\n\nNEW RUN\n\n\n\n)"
-python $code --config 'configs/ablation_SBMs_GraphTransformer_WLPE_PATTERN_500k_sparse_graph_BN.json' --renormalization_pos_enc 0.1
+python $code --config 'configs/ablation_SBMs_GraphTransformer_WLPE_PATTERN_500k_sparse_graph_BN.json' --renormalization_pos_enc 0.01
+python $code --config 'configs/ablation_SBMs_GraphTransformer_WLPE_PATTERN_500k_sparse_graph_BN.json' --renormalization_pos_enc 0.05
+python $code --config 'configs/ablation_SBMs_GraphTransformer_WLPE_PATTERN_500k_sparse_graph_BN.json' --renormalization_pos_enc 0.3
+python $code --config 'configs/ablation_SBMs_GraphTransformer_WLPE_PATTERN_500k_sparse_graph_BN.json' --renormalization_pos_enc 0.7
+python $code --config 'configs/ablation_SBMs_GraphTransformer_WLPE_PATTERN_500k_sparse_graph_BN.json' --renormalization_pos_enc 1.0
+python $code --config 'configs/ablation_SBMs_GraphTransformer_WLPE_PATTERN_500k_sparse_graph_BN.json' --renormalization_pos_enc 3.0
+python $code --config 'configs/ablation_SBMs_GraphTransformer_WLPE_PATTERN_500k_sparse_graph_BN.json' --renormalization_pos_enc 5.0
+python $code --config 'configs/ablation_SBMs_GraphTransformer_WLPE_PATTERN_500k_sparse_graph_BN.json' --renormalization_pos_enc 20.0
