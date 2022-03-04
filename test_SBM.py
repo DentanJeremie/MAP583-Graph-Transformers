a35f4547a6dff4affb05e3dbcd4cbff56b137763
unknown <dan.meller99@gmail.com>
2022-03-04 17:39:53 +0100

    Ajout fichier pour test

---
+++ test_SBM.py
@@ -0,0 +1,333 @@
+"""
+    IMPORTING LIBS
+"""
+import dgl
+
+import numpy as np
+import os
+import socket
+import time
+import random
+import glob
+import argparse, json
+import pickle
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import torch.optim as optim
+from torch.utils.data import DataLoader
+
+from tensorboardX import SummaryWriter
+from tqdm import tqdm
+
+class DotDict(dict):
+    def __init__(self, **kwds):
+        self.update(kwds)
+        self.__dict__ = self
+
+
+"""
+    IMPORTING CUSTOM MODULES/METHODS
+"""
+from nets.SBMs_node_classification.load_net import gnn_model 
+from data.data import LoadData 
+
+
+
+
+"""
+    GPU Setup
+"""
+def gpu_setup(use_gpu, gpu_id):
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu_id)  
+
+    if torch.cuda.is_available() and use_gpu:
+        print('cuda available with GPU:',torch.cuda.get_device_name(0))
+        device = torch.device("cuda")
+    else:
+        print('cuda not available')
+        device = torch.device("cpu")
+    return device
+
+
+
+
+
+
+
+
+
+
+"""
+    VIEWING MODEL CONFIG AND PARAMS
+"""
+def view_model_param(MODEL_NAME, net_params):
+    model = gnn_model(MODEL_NAME, net_params)
+    total_param = 0
+    print("MODEL DETAILS:\n")
+    #print(model)
+    for param in model.parameters():
+        # print(param.data.size())
+        total_param += np.prod(list(param.data.size()))
+    print('MODEL/Total parameters:', MODEL_NAME, total_param)
+    return total_param
+
+
+"""
+    TESTING CODE
+"""
+
+def test_val_pipeline(MODEL_NAME, dataset, params, net_params, dirs,save_model=None,load_model=None):
+    
+    start0 = time.time()
+    per_epoch_time = []
+    
+    DATASET_NAME = dataset.name
+    
+    if net_params['lap_pos_enc']:
+        st = time.time()
+        print("[!] Adding Laplacian positional encoding.")
+        dataset._add_laplacian_positional_encodings(net_params['pos_enc_dim'])
+        print('Time LapPE:',time.time()-st)
+        
+    if net_params['wl_pos_enc']:
+        st = time.time()
+        print("[!] Adding WL positional encoding.")
+        dataset._add_wl_positional_encodings()
+        print('Time WL PE:',time.time()-st)
+    
+    if net_params['full_graph']:
+        st = time.time()
+        print("[!] Converting the given graphs to full graphs..")
+        dataset._make_full_graph()
+        print('Time taken to convert to full graphs:',time.time()-st)
+        
+    testset =  dataset.test
+        
+    root_log_dir, root_ckpt_dir, write_file_name, write_config_file = dirs
+    device = net_params['device']
+    
+    # Write network and optimization hyper-parameters in folder config/
+    with open(write_config_file + '.txt', 'w') as f:
+        f.write("""Dataset: {},\nModel: {}\n\nparams={}\n\nnet_params={}\n\n\nTotal Parameters: {}\n\n"""                .format(DATASET_NAME, MODEL_NAME, params, net_params, net_params['total_param']))
+        
+    
+
+    # setting seeds
+    random.seed(params['seed'])
+    np.random.seed(params['seed'])
+    torch.manual_seed(params['seed'])
+    if device.type == 'cuda':
+        torch.cuda.manual_seed(params['seed'])
+    
+    
+
+    print("Test Graphs: ", len(testset))
+    print("Number of Classes: ", net_params['n_classes'])
+
+    model = gnn_model(MODEL_NAME, net_params)
+    if load_model is not None:
+        print('LOADING MODEL : '+load_model)
+        model.load_state_dict(torch.load(load_model))
+        model.eval()
+    else: 
+        print("WARNING - NO MODEL HAS BEEN SPECIFIED, TESTING RANDOM INITIALIZATION")
+    model = model.to(device)
+
+    
+
+    
+    # import evaluate functions
+    from train.train_SBMs_node_classification import evaluate_network 
+
+
+    test_loader = DataLoader(testset, batch_size=params['batch_size'], shuffle=False, collate_fn=dataset.collate)
+        
+    # At any point you can hit Ctrl + C to break out of training early.
+    try:
+        test_loss, test_acc = evaluate_network(model, device, test_loader, epoch)
+    
+    except KeyboardInterrupt:
+        print('-' * 89)
+        print('Exiting from training early because of KeyboardInterrupt')
+    
+    
+    
+    print("Test Accuracy: {:.4f}".format(test_acc))
+    print("TOTAL TIME TAKEN: {:.4f}s".format(time.time()-start0))
+    
+    
+    return test_loss,test_acc
+
+        
+
+
+
+
+def main():    
+    """
+        USER CONTROLS
+    """
+    
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', help="Please give a config.json file with training/model/data/param details")
+    parser.add_argument('--gpu_id', help="Please give a value for gpu id")
+    parser.add_argument('--model', help="Please give a value for model name")
+    parser.add_argument('--dataset', help="Please give a value for dataset name")
+    parser.add_argument('--out_dir', help="Please give a value for out_dir")
+    parser.add_argument('--seed', help="Please give a value for seed")
+    parser.add_argument('--epochs', help="Please give a value for epochs")
+    parser.add_argument('--batch_size', help="Please give a value for batch_size")
+    parser.add_argument('--init_lr', help="Please give a value for init_lr")
+    parser.add_argument('--lr_reduce_factor', help="Please give a value for lr_reduce_factor")
+    parser.add_argument('--lr_schedule_patience', help="Please give a value for lr_schedule_patience")
+    parser.add_argument('--min_lr', help="Please give a value for min_lr")
+    parser.add_argument('--weight_decay', help="Please give a value for weight_decay")
+    parser.add_argument('--print_epoch_interval', help="Please give a value for print_epoch_interval")    
+    parser.add_argument('--L', help="Please give a value for L")
+    parser.add_argument('--hidden_dim', help="Please give a value for hidden_dim")
+    parser.add_argument('--out_dim', help="Please give a value for out_dim")
+    parser.add_argument('--residual', help="Please give a value for residual")
+    parser.add_argument('--edge_feat', help="Please give a value for edge_feat")
+    parser.add_argument('--readout', help="Please give a value for readout")
+    parser.add_argument('--n_heads', help="Please give a value for n_heads")
+    parser.add_argument('--in_feat_dropout', help="Please give a value for in_feat_dropout")
+    parser.add_argument('--dropout', help="Please give a value for dropout")
+    parser.add_argument('--layer_norm', help="Please give a value for layer_norm")
+    parser.add_argument('--batch_norm', help="Please give a value for batch_norm")
+    parser.add_argument('--self_loop', help="Please give a value for self_loop")
+    parser.add_argument('--max_time', help="Please give a value for max_time")
+    parser.add_argument('--pos_enc_dim', help="Please give a value for pos_enc_dim")
+    parser.add_argument('--lap_pos_enc', help="Please give a value for lap_pos_enc")
+    parser.add_argument('--wl_pos_enc', help="Please give a value for wl_pos_enc")
+    parser.add_argument('--renormalization_pos_enc', help="Please give a value for renormalization_pos_enc")
+    parser.add_argument('--save_model', help="Please give the path for the model to be saved")
+    parser.add_argument('--load_model', help="Please give the path for the model to be loaded")
+    args = parser.parse_args()
+    with open(args.config) as f:
+        config = json.load(f)
+        
+    # device
+    if args.gpu_id is not None:
+        config['gpu']['id'] = int(args.gpu_id)
+        config['gpu']['use'] = True
+    device = gpu_setup(config['gpu']['use'], config['gpu']['id'])
+    # model, dataset, out_dir
+    if args.model is not None:
+        MODEL_NAME = args.model
+    else:
+        MODEL_NAME = config['model']
+    if args.dataset is not None:
+        DATASET_NAME = args.dataset
+    else:
+        DATASET_NAME = config['dataset']
+    dataset = LoadData(DATASET_NAME)
+    if args.out_dir is not None:
+        out_dir = args.out_dir
+    else:
+        out_dir = config['out_dir']
+    # parameters
+    params = config['params']
+    if args.seed is not None:
+        params['seed'] = int(args.seed)
+    
+    # network parameters
+    net_params = config['net_params']
+    net_params['device'] = device
+    net_params['gpu_id'] = config['gpu']['id']
+    net_params['batch_size'] = params['batch_size']
+    if args.L is not None:
+        net_params['L'] = int(args.L)
+    if args.hidden_dim is not None:
+        net_params['hidden_dim'] = int(args.hidden_dim)
+    if args.out_dim is not None:
+        net_params['out_dim'] = int(args.out_dim)   
+    if args.residual is not None:
+        net_params['residual'] = True if args.residual=='True' else False
+    if args.edge_feat is not None:
+        net_params['edge_feat'] = True if args.edge_feat=='True' else False
+    if args.readout is not None:
+        net_params['readout'] = args.readout
+    if args.n_heads is not None:
+        net_params['n_heads'] = int(args.n_heads)
+    if args.in_feat_dropout is not None:
+        net_params['in_feat_dropout'] = float(args.in_feat_dropout)
+    if args.dropout is not None:
+        net_params['dropout'] = float(args.dropout)
+    if args.layer_norm is not None:
+        net_params['layer_norm'] = True if args.layer_norm=='True' else False
+    if args.batch_norm is not None:
+        net_params['batch_norm'] = True if args.batch_norm=='True' else False
+    if args.self_loop is not None:
+        net_params['self_loop'] = True if args.self_loop=='True' else False
+    if args.lap_pos_enc is not None:
+        net_params['lap_pos_enc'] = True if args.pos_enc=='True' else False
+    if args.pos_enc_dim is not None:
+        net_params['pos_enc_dim'] = int(args.pos_enc_dim)
+    if args.wl_pos_enc is not None:
+        net_params['wl_pos_enc'] = True if args.pos_enc=='True' else False
+    if args.renormalization_pos_enc is not None:
+        net_params['renormalization_pos_enc'] = float(args.renormalization_pos_enc)
+
+    try:
+        net_params['renormalization_pos_enc']
+    except KeyError:
+        net_params['renormalization_pos_enc'] = 1.0
+        
+    # SBM
+    net_params['in_dim'] = torch.unique(dataset.train[0][0].ndata['feat'],dim=0).size(0) # node_dim (feat is an integer)
+    net_params['n_classes'] = torch.unique(dataset.train[0][1],dim=0).size(0)
+
+
+    root_log_dir = out_dir + 'logs/' + MODEL_NAME + "_" + DATASET_NAME + "_GPU" + str(config['gpu']['id']) + "_" + time.strftime('%Hh%Mm%Ss_on_%b_%d_%Y')
+    root_ckpt_dir = out_dir + 'checkpoints/' + MODEL_NAME + "_" + DATASET_NAME + "_GPU" + str(config['gpu']['id']) + "_" + time.strftime('%Hh%Mm%Ss_on_%b_%d_%Y')
+    write_file_name = out_dir + 'results/result_' + MODEL_NAME + "_" + DATASET_NAME + "_GPU" + str(config['gpu']['id']) + "_" + time.strftime('%Hh%Mm%Ss_on_%b_%d_%Y')
+    write_config_file = out_dir + 'configs/config_' + MODEL_NAME + "_" + DATASET_NAME + "_GPU" + str(config['gpu']['id']) + "_" + time.strftime('%Hh%Mm%Ss_on_%b_%d_%Y')
+    dirs = root_log_dir, root_ckpt_dir, write_file_name, write_config_file
+
+    if not os.path.exists(out_dir + 'results'):
+        os.makedirs(out_dir + 'results')
+        
+    if not os.path.exists(out_dir + 'configs'):
+        os.makedirs(out_dir + 'configs')
+
+    net_params['total_param'] = view_model_param(MODEL_NAME, net_params)
+    test_val_pipeline(MODEL_NAME, dataset, params, net_params, dirs,args.save_model, args.load_model)
+
+    
+    
+    
+    
+    
+    
+main()    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
