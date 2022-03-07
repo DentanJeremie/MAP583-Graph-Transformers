f4bbf7778f2e3db61406965235df226756fc6a3d
Jérémie Dentan <jeremie.dentan@live.com>
2022-03-07 10:38:48 +0100

    out names and script update

--- scripts/SBMs/script_main_SBMs_node_classification_PATTERN_500k_SizeEm.sh
+++ scripts/SBMs/script_main_SBMs_node_classification_PATTERN_500k_SizeEm.sh
@@ -3,9 +3,6 @@ code=main_SBMs_node_classification.py
 python $code --config 'configs/SBMs_GraphTransformer_LapPE_PATTERN_500k_sparse_graph_BN.json' --lap_pos_enc false --batch_size 20 --epochs 20
 python $code --config 'configs/SBMs_GraphTransformer_LapPE_PATTERN_500k_sparse_graph_BN.json' --pos_enc_dim 1 --batch_size 20 --epochs 20
 python $code --config 'configs/SBMs_GraphTransformer_LapPE_PATTERN_500k_sparse_graph_BN.json' --pos_enc_dim 2 --batch_size 20 --epochs 20
-python $code --config 'configs/SBMs_GraphTransformer_LapPE_PATTERN_500k_sparse_graph_BN.json' --pos_enc_dim 5 --batch_size 20 --epochs 16
-python $code --config 'configs/SBMs_GraphTransformer_LapPE_PATTERN_500k_sparse_graph_BN.json' --pos_enc_dim 10 --batch_size 20 --epochs 16
-python $code --config 'configs/SBMs_GraphTransformer_LapPE_PATTERN_500k_sparse_graph_BN.json' --pos_enc_dim 30 --batch_size 20 --epochs 16
-git add -A
-git commit -m "Run modification pos_enc_dim"
-git push
+python $code --config 'configs/SBMs_GraphTransformer_LapPE_PATTERN_500k_sparse_graph_BN.json' --pos_enc_dim 5 --batch_size 16 --epochs 20
+python $code --config 'configs/SBMs_GraphTransformer_LapPE_PATTERN_500k_sparse_graph_BN.json' --pos_enc_dim 10 --batch_size 16 --epochs 20
+python $code --config 'configs/SBMs_GraphTransformer_LapPE_PATTERN_500k_sparse_graph_BN.json' --pos_enc_dim 30 --batch_size 16 --epochs 20
